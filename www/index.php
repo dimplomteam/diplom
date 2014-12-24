5474abc92b9e0cf774c03089c729fb94804ce3c0
Letowski <mastergame-ate@ya.ru>
2014-12-25 00:36:03 +0300

    first commit

--- www/index.php
+++ www/index.php
@@ -1,2 +1,27 @@
-dfghjkl;sdfg hjhjhj WTF!?
+<?php
+//var_dump($_GET);
+header('Content-Type: text/html; charset=utf-8');
+include("connect.php");
+include("functions.php");
+
+$anchor=trim($_GET["r"],"/");
+if(!$anchor){
+$anchor="index";}
+
+$sql="select * from page where anchor='".$anchor."'";
+$res=mysql_query($sql);
+$page=mysql_fetch_assoc($res);
+
+if(!$page){
+$sql="select * from page where anchor='index'";
+$res=mysql_query($sql);
+$page=mysql_fetch_assoc($res);}
+
+$temp=file_get_contents("temp/template.html");
+
+
+
+
+echo strtr($temp,add_brackets($page));
+
 
