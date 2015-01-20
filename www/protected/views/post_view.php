e9ec7f9b9df0347cc08b484d5e211aa0162b2bf4
Letowski <mastergame-ate@ya.ru>
2015-01-20 21:32:48 +0300

    db

--- www/protected/views/post_view.php
+++ www/protected/views/post_view.php
@@ -10,11 +10,11 @@
 
 
 <div class="article">
-    <h2><span><?=$post->title?></span></h2>
+    <h2><span><?=$this->post->title?></span></h2>
     <div class="clr"></div>
-    <p class="post-data"><span class="date"><?=$post->created_time?></span> &nbsp;
-        |&nbsp; Posted by <a href="/profile/view/<?=$post->user->login?>"><?=$post->user->login?></a>
-        <?=$post->content?>
+    <p class="post-data"><span class="date"><?=$this->post->created_time?></span> &nbsp;
+        |&nbsp; Posted by <a href="/profile/view/<?=$this->post->user->login?>"><?=$this->post->user->login?></a>
+        <?=$this->post->content?>
 
     <div class="clr"></div>
 </div>
