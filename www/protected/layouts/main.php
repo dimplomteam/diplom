f21cf4272033ac491c34914797db8d3fe7ec1778
nasty7722010 <nasty7722010@gmail.com>
2015-01-26 16:48:53 +0300

    jkjk

--- www/protected/layouts/main.php
+++ www/protected/layouts/main.php
@@ -59,15 +59,18 @@
           </div>
           -->
           <div class="gadget">
-            <h2 class="star"><span>Sidebar</span> Menu</h2>
+            <h2 class="star">Навигация</h2>
             <div class="clr"></div>
             <ul class="sb_menu">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="#">TemplateInfo</a></li>
-              <li><a href="#">Style Demo</a></li>
-              <li><a href="#">Blog</a></li>
-              <li><a href="#">Archives</a></li>
-              <li><a href="#">Web Templates</a></li>
+              <li class="active"><a href="#" style="border-radius: 5px;">Сообщения</a></li>
+              <li><a href="#" style="border-radius: 5px;">Мои посты</a></li>
+              <li><a href="#" style="border-radius: 5px;">Мои друзья</a></li>
+              <li><a href="#" style="border-radius: 5px;">Моя лента</a></li>
+              <li><a href="#" style="border-radius: 5px;">Мои подписки</a></li>
+              <li><a href="#" style="border-radius: 5px;">Мои комментарии</a></li>
+              <li><a href="#" style="border-radius: 5px;">Избранное</a></li>
+              <li><a href="#" style="border-radius: 5px;">Настройки</a></li>
+
             </ul>
           </div>
          <!--<div class="gadget">
--- www/protected/views/profile_view.php
+++ www/protected/views/profile_view.php
@@ -5,4 +5,43 @@
  * Date: 20.01.2015
  * Time: 1:21
  */
-var_dump($this->user);
+//var_dump($this->user);
+?>
+<div class="avatar_round">
+    <img src="http://autokadabra.ru/system/uploads/users/1/1938/small.png" alt="avatar" />
+</div>
+
+<div class="menu1">
+    <br id="tab2"/><br id="tab3"/><br id="tab4"/>
+    <a href="#tab1">Профиль</a><a href="#tab2">Рекомендации</a><a href="#tab3">Популярное</a><a href="#tab4">Мои места</a>
+    <div>
+        <h3>
+            Основная информация
+
+        </h3>
+        <p>Имя:</p>
+        <p>Логин:</p>
+        <p>Дата рождения:</p>
+        <p>Пол:</p>
+
+        <h3>
+            Контактная информация
+        </h3>
+        <p>E-mail:</p>
+    </div>
+    <div>вкладка 2</div>
+    <div>вкладка 3</div>
+    <div>вкладка 4</div>
+</div>
+
+
+
+<section class="container">
+    <h2>Форма поиска</h2>
+        <div class="dark">
+        <form>
+            <span><input type="text" class="search rounded" placeholder="Искать..."><input type="button" value="Поиск"></span>
+
+        </form>
+    </div>
+</section>
