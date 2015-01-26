e417562f7e28e8eef3495a6344f0f8b81122dfb2
nasty7722010 <nasty7722010@gmail.com>
2015-01-26 23:08:43 +0300

    jkjk

--- www/protected/views/profile_view.php
+++ www/protected/views/profile_view.php
@@ -11,6 +11,7 @@
     <img src="http://autokadabra.ru/system/uploads/users/1/1938/small.png" alt="avatar" />
 </div>
 
+
 <div class="menu1">
     <br id="tab2"/><br id="tab3"/><br id="tab4"/>
     <a href="#tab1">Профиль</a><a href="#tab2">Рекомендации</a><a href="#tab3">Популярное</a><a href="#tab4">Мои места</a>
@@ -28,12 +29,12 @@
             Контактная информация
         </h3>
         <p>E-mail:</p>
+        <p>Skype:</p>
     </div>
     <div>вкладка 2</div>
     <div>вкладка 3</div>
     <div>вкладка 4</div>
 </div>
-
 
 
 <section class="container">
